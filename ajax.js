4c7277dd05be95c1766ff3b71fae2d13668e00d5
Noreh AD <akinoreh@gmail.com>
2018-04-17 17:22:19 +0300

    .

--- ajax.js
+++ ajax.js
@@ -2,33 +2,6 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
 // https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
 
-var HTTPStatusCodes = {
-    Continue                    : 100,
-    SwitchingProtocols          : 101,
-    Processing                  : 102,
-    EarlyHints                  : 103,
-    
-    OK                          : 200,
-    Created                     : 201,
-    Accepted                    : 202,
-    NonAuthoritativeInformation : 203,
-    NoContent                   : 204,
-    ResetContent                : 205,
-    PartialContent              : 206,
-    MultiStatus                 : 207,
-    AlreadyReported             : 208,
-    
-    BadRequest                  : 400,
-    Unauthorized                : 401,
-    PaymentRequired             : 402,
-    Forbidden                   : 403,
-    NotFound                    : 404,
-    MethodNotAllowed            : 405,
-    NotAcceptable               : 406,
-    
-    
-};
-
 function ajax(url, options) {
     
     options.method   = options.method   || "GET";
@@ -45,7 +18,7 @@ function ajax(url, options) {
     
     if (options.progress) {
         request.onprogress = function (e) {
-            if (request.status == HTTPStatusCodes.OK) {
+            if (request.status == 200) {
                 options.progress.call(request);
             }
         };
@@ -63,15 +36,12 @@ function ajax(url, options) {
             
             if (request.readyState == XMLHttpRequest.DONE) {
                 
-                switch (request.status) {
-                    case HTTPStatusCodes.OK:
-                        if (options.success)
-                            options.success.call(this);
-                        break;
-                    case HTTPStatusCodes.NotFound:
-                        if (options.fail)
-                            options.fail.call(this);
-                        break;
+                if (request.status == 200) {
+                    if (options.success)
+                        options.success.call(this);
+                } else {
+                    if (options.fail)
+                        options.fail.call(this);
                 }
                 
             }
