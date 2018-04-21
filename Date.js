bf953c06fc647bf7b2c8a51b15a3c721e14ea3ff
Noreh AD <akinoreh@gmail.com>
2018-04-21 22:57:47 +0300

    .

---
+++ Date.js
@@ -0,0 +1,34 @@
+Date.prototype.addMinutes = function (m) {
+    this.setMinutes(this.getMinutes() + m);
+    return this;
+};
+
+Date.prototype.time = function () {
+    var h = this.getHours().toString().padStart(2, 0);
+    var m = this.getMinutes().toString().padStart(2, 0);
+    var s = this.getSeconds().toString().padStart(2, 0);
+    return h + ":" + m + ":" + s;
+};
+
+Date.prototype.date = function () {
+    var d = this.getDate().toString().padStart(2, 0);
+    var m = (this.getMonth() + 1).toString().padStart(2, 0);
+    var y = this.getFullYear();
+    return y + "-" + m + "-" + d;
+};
+
+Date.prototype.datetime = function () {
+    return this.date() + " " + this.time();
+};
+
+Date.date = function () {
+    return (new Date()).date();
+};
+
+Date.time = function () {
+    return (new Date()).time();
+};
+
+Date.datetime = function () {
+    return Date.date() + " " + Date.time();
+};
