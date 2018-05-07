01d1474a35970c21f674bd6cbb1e78b176b9ecc3
Noreh AD <akinoreh@gmail.com>
2018-05-07 21:51:03 +0300

    user friendly elapsed time

--- Date.js
+++ Date.js
@@ -43,3 +43,52 @@ Date.time = function () {
 Date.datetime = function () {
     return Date.date() + " " + Date.time();
 };
+
+
+
+Date.times = {
+    s : 1000,
+    m : 1000 * 60,
+    h : 1000 * 60 * 60,
+    d : 1000 * 60 * 60 * 24,
+};
+
+Date.timeStamp2Duration = function ts2duration(ts) {
+    var duration = {
+        s : null,
+        m : null,
+        h : null,
+        d : null,
+    };
+    
+    var remainder = 0;
+    
+    duration.d = Math.floor(ts / Date.times.d);
+    remainder = ts % Date.times.d;
+    if (duration.d == 0) remainder = ts;
+    
+    duration.h = Math.floor(remainder / Date.times.h);
+    remainder = remainder % Date.times.h;
+    if (duration.h == 0) remainder = ts;
+    
+    duration.m = Math.floor(remainder / Date.times.m);
+    remainder = remainder % Date.times.m;
+    if (duration.m == 0) remainder = ts;
+    
+    duration.s = Math.floor(remainder / Date.times.s);
+    remainder = remainder % Date.times.s;
+    
+    return duration;
+};
+    
+Date.duration2String = function duration2string(duration, after) {
+    var str = [];
+    if (duration.d) str.push(duration.d + " days");
+    if (duration.h) str.push(duration.h + " hours");
+    if (duration.m) str.push(duration.m + " minutes");
+    // if (duration.s) str.push(duration.s + " seconds");
+    if (after) {
+        str.push(after);
+    }
+    return str.join(" ");
+};
