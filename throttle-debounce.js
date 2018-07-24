2ab3e98adb8fb5bbe5df2f3869e2e9c13519550d
Noreh AD <akinoreh@gmail.com>
2018-07-24 12:31:42 +0300

    .

---
+++ throttle-debounce.js
@@ -0,0 +1,24 @@
+// https://stackoverflow.com/questions/27078285/simple-throttle-in-js/51493084#51493084
+
+function throttle(callback, delay) {
+    var timeoutHandler = null;
+    return function () {
+        if (timeoutHandler == null) {
+            timeoutHandler = setTimeout(function () {
+                callback();
+                clearInterval(timeoutHandler);
+                timeoutHandler = null;
+            }, delay);
+        }
+    }
+}
+
+function debounce(callback, delay) {
+    var timeoutHandler = null;
+    return function () {
+        clearTimeout(timeoutHandler);
+        timeoutHandler = setTimeout(function () {
+            callback();
+        }, delay);
+    }
+}
