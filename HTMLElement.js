f4fdcb131e23fd3ec1c18b14707f8e607a6f32cf
Noreh AD <akinoreh@gmail.com>
2018-10-09 13:56:22 +0300

    .

--- HTMLElement.js
+++ HTMLElement.js
@@ -9,6 +9,53 @@ function dashed2camelCase(str) {
     return words[0];
 }
 
+function getPos(element) {
+    var top  = 0;
+    var left = 0;
+    do {
+        top  += element.offsetTop  || 0;
+        left += element.offsetLeft || 0;
+        element = element.offsetParent;
+    } while(element);
+    return { top, left };
+};
+
+function inView(el) {
+    var rect = el.getBoundingClientRect();
+    var el = {
+        top    : rect.top + scrollY,
+        bottom : rect.top + scrollY + rect.height,
+        height : rect.height,
+    };
+    var view = {
+        top    : scrollY,
+        bottom : scrollY + innerHeight,
+    };
+    var result = {
+        visible : "none",
+        amount  : 0,
+    };
+    if (el.top < view.bottom && el.top > view.top) {
+        if (el.bottom < view.bottom) {
+            result.visible = "all";
+            result.amount = 1;
+        } else {
+            result.visible = "top";
+            result.amount = (view.bottom - el.top) / el.height;
+        }
+    }
+    else if (el.bottom > view.top && el.top < view.bottom) {
+        if (el.top > view.top) {
+            result.visible = "all";
+            result.amount = 1;
+        } else {
+            result.visible = "bottom";
+            result.amount = (el.bottom - view.top) / el.height;
+        }
+    }
+    return result;
+}
+
 function style(element, property, value) {
     if (property) {
         if (value) {
