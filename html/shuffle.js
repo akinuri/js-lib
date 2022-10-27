c0f4721185d42ffe608950a392f38a901fb93cfd
Noreh AD <akinoreh@gmail.com>
2022-10-27 21:26:36 +0300

    html children shuffle fix

--- html/shuffle.js
+++ html/shuffle.js
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/11972692/2202732
 function shuffleChildren(parent) {
-    for (var i = parent.children.length; i >= 0; i--) {
-        parent.appendChild(parent.children[Math.round(Math.random() * i) | 0]);
+    for (var i = parent.children.length - 1; i >= 0; i--) {
+        parent.appendChild(parent.children[Math.round(Math.random() * i)]);
     }
 }
