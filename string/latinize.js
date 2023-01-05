f0f4fb92c262148c416cc8412dc17cae8c17e069
Noreh AD <akinoreh@gmail.com>
2023-01-05 12:58:22 +0300

    latinize

--- string/latinize.js
+++ string/latinize.js
@@ -1,12 +1,54 @@
-String.prototype.latinize = function latinize(locale) {
-    var str = this;
-    var chars = [
-        ["ç", "c"], ["ğ", "g"], ["ı", "i"], ["ö", "o"], ["ş", "s"], ["ü", "u"],
-        ["Ç", "C"], ["Ğ", "G"], ["İ", "I"], ["Ö", "O"], ["Ş", "S"], ["Ü", "U"],
-        ["i̇", "i"], ["ü", "u"],
-    ];
-    chars.forEach(function (c) {
-        str = str.replace(c[0], c[1]);
-    });
-    return str;
+// https://en.wikipedia.org/wiki/Transliteration
+// https://en.wikipedia.org/wiki/Romanization
+// https://stackoverflow.com/a/34867642/2202732
+
+let LATINIZATION_MAP = {
+    "a"  : "à á â ã ä å æ",
+    "c"  : "ç",
+    "d"  : "ð",
+    "e"  : "è é ê ë",
+    "g"  : "ğ",
+    "i"  : "ì í î ï i̇ ı",
+    "m"  : "μ",
+    "n"  : "ñ",
+    "o"  : "ò ó ô õ ö ø",
+    "oe" : "œ",
+    "s"  : "ş",
+    "sh" : "š",
+    "th" : "þ",
+    "u"  : "ù ú û ü",
+    "y"  : "ý ÿ",
+    
+    "A"  : "À Á Â Ä Å Æ Ã Ā",
+    "C"  : "Ç",
+    "D"  : "Ð",
+    "E"  : "È É Ê Ë",
+    "G"  : "Ğ",
+    "I"  : "Ì Í Î Ï İ",
+    "N"  : "Ñ",
+    "O"  : "Ò Ó Ô Õ Ö Ø",
+    "OE" : "Œ",
+    "S"  : "ß Ş",
+    "SH" : "Š",
+    "TH" : "Þ",
+    "U"  : "Ù Ú Û Ü",
+    "Y"  : "Ý Ÿ",
+};
+
+function regexifyLatinizationMap() {
+    for (let targetChar in LATINIZATION_MAP) {
+        let cases = LATINIZATION_MAP[targetChar];
+        cases = cases.replaceAll(" ", "|");
+        cases = new RegExp(cases);
+        LATINIZATION_MAP[targetChar] = cases;
+    }
+}
+regexifyLatinizationMap();
+
+function latinize(text) {
+    for (let targetChar in LATINIZATION_MAP) {
+        let cases = LATINIZATION_MAP[targetChar];
+        text = text.replace(cases, targetChar);
+    }
+    return text;
 }
