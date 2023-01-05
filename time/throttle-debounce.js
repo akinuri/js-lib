9025b0b8f1f428cb35000f0b65d16482a8f62dbe
Noreh AD <akinoreh@gmail.com>
2023-01-05 14:28:54 +0300

    throttle & debounce

--- time/throttle-debounce.js
+++ time/throttle-debounce.js
@@ -1,25 +1,34 @@
 // https://stackoverflow.com/questions/27078285/simple-throttle-in-js/51493084#51493084
 
-function throttle(callback, delay) {
-    var timeoutHandler = null;
-    return function throttleInner() {
+function throttle(callback, delay = 1000) {
+    var timeoutHandle = null;
+    let throttleInner = function throttleInner() {
         let passedArgs = Array.from(arguments);
-        if (timeoutHandler == null) {
-            timeoutHandler = setTimeout(function () {
+        if (timeoutHandle == null) {
+            // NOTE: should the first call wait for the delay?
+            timeoutHandle = setTimeout(function () {
                 callback.apply(this, passedArgs);
-                timeoutHandler = null;
-            }.bind(this), delay);
+                timeoutHandle = null;
+            }.bind(this), delay || 1000);
         }
-    }.bind(this);
+    };
+    if (!(this instanceof Window)) {
+        throttleInner = throttleInner.bind(this);
+    }
+    return throttleInner;
 }
 
-function debounce(callback, delay) {
-    var timeoutHandler = null;
-    return function debounceInner() {
+function debounce(callback, delay = 1000) {
+    var timeoutHandle = null;
+    let debounceInner = function debounceInner() {
         let passedArgs = Array.from(arguments);
-        clearTimeout(timeoutHandler);
-        timeoutHandler = setTimeout(function () {
+        clearTimeout(timeoutHandle);
+        timeoutHandle = setTimeout(function () {
             callback.apply(this, passedArgs);
-        }.bind(this), delay);
-    }.bind(this);
+        }.bind(this), delay || 1000);
+    };
+    if (!(this instanceof Window)) {
+        debounceInner = debounceInner.bind(this);
+    }
+    return debounceInner;
 }
