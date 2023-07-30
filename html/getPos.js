bd0e8d6d798f74008a036f9524d1b560fb11c8af
Noreh AD <akinoreh@gmail.com>
2023-07-30 15:00:33 +0300

    abs & rel pos

--- html/getPos.js
+++ html/getPos.js
@@ -1,10 +1,18 @@
-function getPos(element) {
-    var top  = 0;
-    var left = 0;
+function getAbsPos(element) {
+    let top  = 0;
+    let left = 0;
     do {
         top  += element.offsetTop  || 0;
         left += element.offsetLeft || 0;
         element = element.offsetParent;
     } while(element);
     return { top, left };
-};
+}
+
+function getRelPos(child, parent) {
+    let parentAbsPos = getAbsPos(parent || child.parentElement);
+    let childAbsPos  = getAbsPos(child);
+    let top  = childAbsPos.top - parentAbsPos.top;
+    let left = childAbsPos.left - parentAbsPos.left;
+    return { top, left };
+}
