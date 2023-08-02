daf0531686d3ab6c54f2ff683a67bab3efe8af15
Noreh AD <akinoreh@gmail.com>
2023-08-02 20:30:30 +0300

    createSlug

---
+++ string/createSlug.js
@@ -0,0 +1,73 @@
+// https://en.wikipedia.org/wiki/Clean_URL#Slug
+// https://en.wikipedia.org/wiki/Slug_(publishing)
+// https://www.semrush.com/blog/what-is-a-url-slug/
+// https://en.wikipedia.org/wiki/URI_normalization
+function createSlug(string, userOptions) {
+    
+    let defaultOptions = {
+        lowercase : true,
+        latinize  : true,
+        normalize : true,
+        nopunc    : true,
+        case      : "kebab",
+        strip     : [],
+    };
+    
+    let options = {...defaultOptions, ...userOptions};
+    
+    if (options.latinize) {
+        string = latinize(string);
+    }
+    
+    if (options.lowercase) {
+        string = string.toLowerCase();
+    }
+    
+    if (options.normalize) {
+        string = createSlug.normalize(string);
+    }
+    
+    if (options.nopunc) {
+        string = createSlug.purgePunctuation(string);
+    }
+    
+    if (["kebab", "snake"].includes(options.case)) {
+        string = string.split(/\s+/);
+        if      (options.case == "kebab") string = string.join("-");
+        else if (options.case == "snake") string = string.join("_");
+    }
+    
+    if (options.strip) {
+        for (let letter of options.strip) {
+            string = string.replaceAll(letter, "");
+        }
+    }
+    
+    return string;
+}
+
+createSlug.normalizationMap = [
+    ["-", " "],
+    ["—", " "],
+    ["&", " and "],
+];
+
+createSlug.normalize = function (str) {
+    for (let map of createSlug.normalizationMap) {
+        str = str.replaceAll(map[0], map[1]);
+    }
+    return str;
+};
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa365247(v=vs.85).aspx#Naming_Conventions
+createSlug.punctuationChars = [
+    "<", ">", ":", "\"", "/", "\\", "|", "?", "*",
+    "'", "¦", ".", ",", "!",
+];
+
+createSlug.purgePunctuation = function (str) {
+    for (let map of createSlug.punctuationChars) {
+        str = str.replaceAll(map[0], map[1]);
+    }
+    return str;
+};
