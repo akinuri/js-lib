fc40196f1daa66762694a4261a5641dc65cc84e1
Noreh AD <akinoreh@gmail.com>
2023-08-11 21:15:21 +0300

    loop

--- number/loop.js
+++ number/loop.js
@@ -1,35 +1,37 @@
-function loop(min, max) {
-    var min = min || 0;
-    var max = max || 0;
-    var num = this % max;
-    if (num < min) {
-        num += max;
+/* 
+    loopType
+        open  : first and the last values are NOT the same, e.g. 1-7 (days of week)
+        close : first and the last values are the same, e.g. 0-360 degrees
+ */
+function loop(value, min, max, loopType = "open") {
+    if (!["open", "close"].includes(loopType)) {
+        throw new Error("Invalid loop type. Use 'open' or 'close'.");
     }
-    return num;
+    let loopTypeOffset = loopType == "open" ? 1 : 0;
+    let rangeSize = max - min + loopTypeOffset;
+    value = (value - min) % rangeSize + min;
+    if (value < min) {
+        value += rangeSize;
+    }
+    return value;
 }
 
-// min and max are inclusive
-function loop2(min, max) {
-    var min = min || 0;
-    var max = max || 0;
-    var result = this;
-    var dist = Math.abs(max - min) + 1;
-    if (this < min) {
-        var underflow = Math.abs(this - min);
-        var remainder = underflow % dist;
-        if (remainder == 0) {
-            result = min;
-        } else {
-            result = max - remainder + 1;
-        }
-    } else if (this > max) {
-        var overflow = this - max;
-        var remainder = overflow % dist;
-        if (remainder == 0) {
-            result = max;
-        } else {
-            result = min + remainder - 1;
-        }
-    }
-    return result.valueOf();
-};
+// some tests
+
+// let loopStart = 0;
+// let loopEnd   = 360;
+// let loopType  = "close";
+// let loopRange = Math.abs(loopStart - loopEnd);
+// let increase  = 1 * 60;
+// for (let i = loopStart-loopRange*2; i < loopEnd+loopRange*2; i+=increase) {
+    // console.log( i, loop(i, loopStart, loopEnd, loopType) );
+// }
+
+// let loopStart = 1;
+// let loopEnd   = 7;
+// let loopType  = "open";
+// let loopRange = Math.abs(loopStart - loopEnd);
+// let increase  = 1 * 1;
+// for (let i = loopStart-loopRange*2; i < loopEnd+loopRange*2; i+=increase) {
+    // console.log( i, loop(i, loopStart, loopEnd, loopType) );
+// }
