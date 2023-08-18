6d1d9bac2278533f7eb57927b2494f8d1916540e
Noreh AD <akinoreh@gmail.com>
2023-08-18 21:49:18 +0300

    searchParams

---
+++ url/URLSearchParameters.js
@@ -0,0 +1,26 @@
+function removeURLSearchParameters(url) {
+    let isObj = url instanceof URL;
+    if (!isObj) {
+        url = new URL(url);
+    }
+    let _url = url.origin + url.pathname;
+    if (isObj) {
+        return new URL(_url);
+    }
+    return _url;
+}
+
+function addURLSearchParameters(url, parameters) {
+    let isObj = url instanceof URL;
+    if (!isObj) {
+        url = new URL(url);
+    }
+    for (const key in parameters) {
+        const value = parameters[key];
+        url.searchParams.append(key, value);
+    }
+    if (isObj) {
+        return url;
+    }
+    return url.toString();
+}
