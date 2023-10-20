e65912e737aa230864083ba5300e86ee17ceed07
Noreh AD <akinoreh@gmail.com>
2023-10-20 19:41:53 +0300

    qs(): accept string parent

--- html/qs.js
+++ html/qs.js
@@ -1,21 +1,27 @@
 function qs(query, parent) {
     if (query instanceof HTMLElement) return query;
-    if (parent === null) {
-        return null;
-    }
     if (parent === undefined) {
         parent = document;
+    }
+    if (typeof parent == "string") {
+        parent = qs(parent);
+    }
+    if (parent === null) {
+        return null;
     }
     return parent.querySelector(query);
 }
 
 function qsa(query, parent) {
     if (query instanceof HTMLElement) return query;
-    if (parent === null) {
-        return [];
-    }
     if (parent === undefined) {
         parent = document;
+    }
+    if (typeof parent == "string") {
+        parent = qs(parent);
+    }
+    if (parent === null) {
+        return [];
     }
     return Array.from(parent.querySelectorAll(query));
 }
