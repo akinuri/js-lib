d40bdb976d9930cc00abc73559cda4617ecd531a
Noreh AD <akinoreh@gmail.com>
2023-12-07 21:34:24 +0300

    getObjProp

--- object/getObjProp.js
+++ object/getObjProp.js
@@ -1,6 +1,13 @@
-/**
- * Safe object property access.
- */
-function getObjProp(obj, prop, fallback = null) {
-    return obj.hasOwnProperty(prop) ? obj.prop : fallback;
+function getObjProp(obj, path, fallback) {
+    const pathProps = path.split(".");
+    let prop = obj;
+    for (let i = 0; i < pathProps.length; i++) {
+        const pathProp = pathProps[i];
+        if (prop && prop.hasOwnProperty(pathProp)) {
+            prop = prop[pathProp];
+        } else {
+            return fallback;
+        }
+    }
+    return prop;
 }
