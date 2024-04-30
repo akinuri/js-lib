5360c8b889a940b2682a1800f1141d4568b232bf
Noreh AD <akinoreh@gmail.com>
2024-04-30 22:16:09 +0300

    array/shuffle fix

--- array/shuffle.js
+++ array/shuffle.js
@@ -1,8 +1,8 @@
-function shuffle(...items) {
-    for (let i = items - 1; i > 0; i--) {
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
         let random    = Math.floor(Math.random() * (i + 1));
-        let temp      = items[i];
-        items[i]      = items[random];
-        items[random] = temp;
+        let temp      = array[i];
+        array[i]      = array[random];
+        array[random] = temp;
     }
 }
