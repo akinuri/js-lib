d9e6030d9a86c397d1d3a3e398f9b94ffb06b2e6
Noreh AD <akinoreh@gmail.com>
2024-05-01 21:05:12 +0300

    math/sum: flexible args

--- math/sum.js
+++ math/sum.js
@@ -1,5 +1,6 @@
 function sum() {
-    return Array.from(arguments).reduce(
+    let numbers = Array.from(arguments).flat(Infinity);
+    return numbers.reduce(
         function (sum, currentValue) {
             return sum + (parseFloat(currentValue) || 0);
         },
